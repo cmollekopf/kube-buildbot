8855794e9b60a01606eb1abaae6492e68c6cb96a
Christian Mollekopf <mollekopf@kolabsystems.com>
2017-10-11 16:17:44 +0200

    More benchmarks

--- master/benchmarkdashboard.py
+++ master/benchmarkdashboard.py
@@ -23,24 +23,23 @@ benchmarkdashboard.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @benchmarkdashboard.route("/index.html")
 def main():
+    benchmarksToRun = ["mail_query", "mail_query_threadleader"]
     charts = []
-    output = subprocess.check_output("{}/testenv.py srcbuild --noninteractive kube Sink hawd json mail_query".format(config.dockerdir), shell=True, stderr=subprocess.STDOUT)
-    log.msg("Output: ", output)
-    if output:
-        hawdResult = json.loads(output)
-        datasetName = hawdResult['dataset']
-        description = hawdResult['description']
-        graph_data = []
-        for i, row in enumerate(sorted(hawdResult['rows'], key=lambda row: row['timestamp'])):
-            commit = row['commit']
-            value = row['queryResultPerMs']
-            dataset = row['rows']
-            timestamp = row['timestamp']
-            graph_data.append(dict(x=i, y=value))
 
-        charts.append({"name": "mail_query",
-                       "foo": "bar",
-                       "data": graph_data})
+    for benchmark in benchmarksToRun:
+        output = subprocess.check_output("{}/testenv.py srcbuild --noninteractive kube Sink hawd json {}".format(config.dockerdir, benchmark), shell=True, stderr=subprocess.STDOUT)
+        # log.msg("Output: ", output)
+        if output:
+            hawdResult = json.loads(output)
+            graph_data = []
+            for i, row in enumerate(sorted(hawdResult['rows'], key=lambda row: row['timestamp'])):
+                # commit = row['commit']
+                graph_data.append(dict(x=i, y=row['queryResultPerMs']))
+
+            charts.append({"name": hawdResult['dataset'],
+                        "description": hawdResult['description'],
+                        "foo": "bar",
+                        "data": graph_data})
 
     if charts:
         return render_template('mydashboard.html', charts=charts)
